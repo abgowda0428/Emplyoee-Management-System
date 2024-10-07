a5c11ec870c245805aac6fd760d09f3806d8f7c0
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-10-07 16:24:28 +0530

    Intial Commit

--- src/app/Components/roles/roles.component.ts
+++ src/app/Components/roles/roles.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+
+  first_name:String = "Shravana U Naik";
+  last_name = "Polar";
+  Mobile_number : Number = 8217665338;
+  isActive : boolean = false;
+  inputtype:string = "radio"
+  selectedState : string ="";
+
+showAlert(){
+  alert("Welcome to Angular Toutrial")
+}
+
+showAlerttwo(message:string){
+  alert(message)
+}
 
 }
